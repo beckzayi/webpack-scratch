5f4b90d4f58c49e1021fd4746e016b2bb280b8fc
Darren Li <dxl@enablehr.com>
2021-06-11 14:46:10 +1000

    implement CommonJS require() function

--- b.js
+++ b.js
@@ -1,3 +1,34 @@
-let str = require('./a');
+const fs = require('fs');
+
+/**
+ * Implement CommonJS 'require'
+ * @param {string} moduleName
+ * @returns function
+ */
+function req(moduleName) {
+  let content = fs.readFileSync(moduleName);
+  let fn = new Function(
+    'exports',
+    'module',
+    '__dirname',
+    '__filename',
+    content + '\n return module.exports'
+  );
+
+  let module = {
+    exports: {},
+  };
+
+  /**
+   * function (exports, module, __dirname, __filename) {
+   *    // content + '\n return module.exports'
+   *    module.exports = 'Welcome';
+   *    return module.exports;
+   * }
+   */
+  return fn(module.exports, module, __dirname, __filename);
+}
+
+let str = req('./a.js');
 
 console.log(str);
